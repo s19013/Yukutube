d2adc699ce55939490a855f67484974036a16d68
s19013 <s19013@std.it-college.ac.jp>
2020-02-25 05:47:52 +0000

    Updates

---
+++ precache-manifest.5f46ac9eca7a14f1a6e35367db1a1a77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1978074ca355f87d501edda38364b258",
+    "url": "/Yukutube/index.html"
+  },
+  {
+    "revision": "276565b857f378570d97",
+    "url": "/Yukutube/static/css/main.d8084880.chunk.css"
+  },
+  {
+    "revision": "c13b59b2ff9722ffe57a",
+    "url": "/Yukutube/static/js/2.92d79c12.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Yukutube/static/js/2.92d79c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276565b857f378570d97",
+    "url": "/Yukutube/static/js/main.3ecc01c7.chunk.js"
+  },
+  {
+    "revision": "033b2fa86d89f88c61ad",
+    "url": "/Yukutube/static/js/runtime-main.4b522d29.js"
+  }
+]);
