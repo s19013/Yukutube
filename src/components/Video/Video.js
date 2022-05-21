825a2f5bd45c6b68cb172cc850af2635d5fad340
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-21 23:21:28 +0900

    コメント｡リファクタリング

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './Video.css'
 
 const Video = (props) => {
+  //読み込み中の処理
   if (!props.video) {
     return(
         <div className="loader-wrap">
           <div className="loader">Loading...</div>
         </div>
     )
-    // className="embed-responsive-item"
   }
-    const videoURL = 'https://www.youtube.com/embed/' + props.video.id.videoId;
-    return(
+  // 読み込み完了後の処理
+  const videoURL = 'https://www.youtube.com/embed/' + props.video.id.videoId;
+  return(
     <div className="Video">
         <div className="Player">
           <div className="embed-responsive embed-responsive-16by9" >
@@ -26,9 +27,7 @@ const Video = (props) => {
         <h2>{props.video.snippet.title}</h2>
       </div>
     </div>
-    )
+  )
 }
 
 export default Video;
-{/* <div className="root">
-  <div className="video" > */}
