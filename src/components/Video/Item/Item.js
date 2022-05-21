27ad7471d9e56d35ee7955630d1fcdcd24b8e45d
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-21 23:05:54 +0900

    リファクタリング

--- src/components/Video/Item/Item.js
+++ src/components/Video/Item/Item.js
@@ -1,15 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './Item.css';
 
 const Item = (props) => {
   return(
-    <li className="list"
-      onClick={() => props.onVideoClicked(props.video)}
-    >
+    <li className="list" onClick={() => props.onVideoClicked(props.video)}>
       <div className="oneItem">
         <div className="images">
-          <img
-             src={props.video.snippet.thumbnails.medium.url}/>
+          <img src={props.video.snippet.thumbnails.medium.url}/>
         </div>
         <div className="info">
           <h5>{props.video.snippet.title}</h5>
@@ -20,15 +18,3 @@ const Item = (props) => {
 }
 
 export default Item
-// <li className="list-group-item"
-//   onClick={() => props.onVideoClicked(props.video)}
-// >
-//   <div className="video-list media">
-//     <div className="media-left">
-//       <img className="mr-3" src={props.video.snippet.thumbnails.default.url}/>
-//     </div>
-//   </div>
-//   <div className="media-body">
-//     <h5 className="media-heading">{props.video.snippet.title}</h5>
-//   </div>
-// </li>
