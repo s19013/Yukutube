7dfcd00d333fb903e28be1ce424740916dca8a72
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-22 00:04:57 +0900

    コメント追加｡リファクタリング

--- src/App.js
+++ src/App.js
@@ -5,23 +5,17 @@ import Body from './components/Body/Body'
 import List from './components/Video/List/List'
 import Video from './components/Video/Video'
 import YSearch from 'youtube-api-search';
-const API = 'AIzaSyCYWwP0ZWt9ENzxJR0vRq-onq1nH1NOBWQ'
+const API = 'AIzaSyAR8-LzGCEDEbga4PXzdgfssJ_HENLdSk8'
 
 const App = () => {
   const d = new Date()
-  const [newTerm,setNewTerm] = useState("")
-  const [newMaxResults,setNewMaxResults] = useState(5)
-  const [newOrder,setNewOrder] = useState("relevance")
-  // const [BDate,setBDate] = useState(d.getDate())
-  // const [ADate,setADate] = useState(d.getDate())
-  // const [BMonth,setBMonth] = useState(d.getMonth())
-  // const [AMonth,setAMonth] = useState(d.getMonth())
-  // const [BYear,setBYear] = useState(d.getFullYear())
-  // const [AYear,setAYear] = useState(d.getFullYear()-10)
-  const [BDate,setBDate] = useState([d.getFullYear(),d.getMonth()+1,d.getDate()])
-  const [ADate,setADate] = useState([2005,1,1])
-  const [videos,setVideos] = useState([])
-  const [selectedVideo,setSelectedVideo] = useState(null)
+  const [newTerm,setNewTerm] = useState("") //検索キーワード
+  const [newMaxResults,setNewMaxResults] = useState(5) //最大表示数
+  const [newOrder,setNewOrder] = useState("relevance") //並び方
+  const [BDate,setBDate] = useState([d.getFullYear(),d.getMonth()+1,d.getDate()]) //検索範囲はじめ
+  const [ADate,setADate] = useState([d.getFullYear()-5,d.getMonth()+1,d.getDate()]) //検索範囲終わり
+  const [videos,setVideos] = useState([]) //取ってきたビデオ達
+  const [selectedVideo,setSelectedVideo] = useState(null) //選んだビデオ(すぐ再生できるビデオ)
   useEffect(() => {
       YSearch({key:API,term: 'ゆっくり',maxResults:newMaxResults}, (data) => {
         setVideos(data)
@@ -31,25 +25,22 @@ const App = () => {
       setNewTerm("ゆっくり ")
   },[])
 
+
 // リストを取ってきていじってsetBDate
-
 // handler
-  const onVideoClickedHandler = (video) => {
-    setSelectedVideo(video)
-  }
+  //再生する動画変更
+  const onVideoClickedHandler = (video) => {setSelectedVideo(video)}
 
-  const onKeywordChangedHandler = (keyword) => {
-    setNewTerm("ゆっくり " + keyword)
-  }
+  //キーワード変更
+  const onKeywordChangedHandler = (keyword) => {setNewTerm("ゆっくり " + keyword)}
 
-  const onChangedMaxResultsHandler = (MRvalue) => {
-    setNewMaxResults(MRvalue)
-  }
+  //最大表示数変更
+  const onChangedMaxResultsHandler = (MRvalue) => {setNewMaxResults(MRvalue)}
 
-  const onChangedOrderHandler = (O) => {
-    setNewOrder(O)
-  }
+  // 並び替え変更
+  const onChangedOrderHandler = (Order) => {setNewOrder(Order)}
 
+  //検索開始範囲変更
   const onChangeBDateHandler = (id,num) => {
     var newBDate = BDate
     newBDate[id] = num
@@ -61,69 +52,40 @@ const App = () => {
     newADate[id] = num
     setADate(newADate)
   }
-// 日付
-  // const onChangedBDate = (BD) => {
-  //   setBDate(BD)
-  // }
-  //
-  // const onChangedADate = (AD) => {
-  //   setADate(AD)
-  // }
-  //
-  // const onChangedBMonth = (BM) => {
-  //   setBMonth(BM)
-  // }
-  //
-  // const onChangedAMonth = (AM) => {
-  //   setAMonth(AM)
-  // }
-  //
-  // const onChangedBYear = (BY) => {
-  //   setBYear(BY)
-  // }
-  //
-  // const onChangedBYear = (BY) => {
-  //   setBYear(BY)
-  // }
 
+  //最終日を正す
   const check = () => {
     var newBDate = BDate
     var newADate = ADate
     const BM = newBDate[1]
     const AM = newADate[1]
     if (BM === 1 || BM === 3 || BM === 5 || BM === 7 || BM === 8 || BM === 10 || BM === 12) {
-      if (newBDate[2] === 31) {
-        newBDate[2] = 30
-      }
+      if (newBDate[2] === 31) {newBDate[2] = 30}
     }
     if (AM === 1 || AM === 3 || AM === 5 || AM === 7 || AM === 8 || AM === 10 || AM === 12) {
-      if (newADate[2] === 31) {
-        newADate[2] = 30
-      }
+      if (newADate[2] === 31) {newADate[2] = 30}
     }
     // 閏年
     if (BM === 2 && (newBDate[2] > 29)) {
-      if ((newBDate[0]%4 === 0) && (newBDate[0]%100 !== 0) || (newBDate[0]%400 === 0)) {
-        newBDate[2] = 29
-      }
+      // eslint-disable-next-line no-mixed-operators
+      if ((newBDate[0]%4 === 0) && (newBDate[0]%100 !== 0) || (newBDate[0]%400 === 0)) {newBDate[2] = 29}
       else newBDate[2] = 28
     }
     if (AM === 2 && (newADate[2] > 29)) {
-      if ((newADate[0]%4 === 0) && (newADate[0]%100 !== 0) || (newADate[0]%400 === 0)) {
-        newADate[2] = 29
-      }
+      // eslint-disable-next-line no-mixed-operators
+      if ((newADate[0]%4 === 0) && (newADate[0]%100 !== 0) || (newADate[0]%400 === 0)) {newADate[2] = 29}
       else newADate[2] = 28
     }
-    //
+    //値更新
     setBDate(newBDate)
     setADate(newADate)
     search()
   }
 
+  //検索開始
   const search = () => {
-    if (newTerm === '') {
-      setNewTerm('ゆっくり ')
-    }
+    //初期の検索ワード 空の時
+    if (newTerm === '') {setNewTerm('ゆっくり ')}
     YSearch({ key: API,
       term: newTerm,
       maxResults:newMaxResults,
@@ -131,12 +93,11 @@ const App = () => {
       publishedBefore:`${Number(BDate[0])}-${Number(('0' + BDate[1]).slice(-2))}-${Number(('0' + BDate[2]).slice(-2))}T23:59:59Z`,
       publishedAfter:`${Number(ADate[0])}-${Number(('0' + ADate[1]).slice(-2))}-${Number(('0' + ADate[2]).slice(-2))}T00:00:00Z`,
     },
-     (data) => {
-      setVideos(data)
-      setSelectedVideo(data[0])
+     (data) => { //データを受け取る
+      setVideos(data) //動画保存
+      setSelectedVideo(data[0]) 
     })
   }
-
 
   return(
     <div className = "App">
