a821b739c30c1900f31fbbbd82a1ff3a9a9e4696
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-22 12:15:08 +0900

    Updates

---
+++ precache-manifest.98561ad7e9ee79e6cb45737bf2afb0cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdbd7b5026bc70503a57b602cf0f16e",
+    "url": "/Yukutube/index.html"
+  },
+  {
+    "revision": "895f9ede06982c79c343",
+    "url": "/Yukutube/static/css/main.51534b44.chunk.css"
+  },
+  {
+    "revision": "c411cabfd635019de504",
+    "url": "/Yukutube/static/js/2.a77cad7f.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Yukutube/static/js/2.a77cad7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895f9ede06982c79c343",
+    "url": "/Yukutube/static/js/main.755221ae.chunk.js"
+  },
+  {
+    "revision": "50821af40aa40ffbdbdd",
+    "url": "/Yukutube/static/js/runtime-main.03c688dc.js"
+  }
+]);
